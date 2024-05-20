3cfa6b31c0b1f1f264a900c49b2359baffcd579c
suresh <sureshkmr123789@gmail.com>
2024-05-20 18:32:55 +0530

    shell practice

--- package_install.sh
+++ package_install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-# sudo su -
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then 
+    echo "you are super user"
+else
+    echo "you must be super user to execute"
+    exit1
+fi
+
 dnf install mysql -y
-$?
+
+if [ $? -eq 0 ]
+then
+    echo "installation success"
+else
+    echo "installation FAILURE"
+    exit1
+fi
