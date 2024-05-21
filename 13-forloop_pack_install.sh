294558403ccd2a5cfc73561680fa7a7c912be692
suresh <sureshkmr123789@gmail.com>
2024-05-21 21:41:30 +0530

    shell practice

---
+++ 13-forloop_pack_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d '.' -f1 )
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .....$R FAILED $N"
+    else
+        echo -e "$2 .....$G SUCESS $N"
+    fi
+}
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -eq 0 ]
+then 
+    echo -e "$G you are super user $N"
+else
+    echo -e "$R you must be super user to execute $N"
+    exit 1
+fi
+
+for i in $@
+do
+    dnf installed list $i
+    if [ $? -ne 0 ]
+    then
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "$i installation"
+    else
+        echo "$i already installed .... $Y SKIPPING $N"
+    fi
+done
