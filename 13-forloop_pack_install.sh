385d549711cd11165d2713f019a0d24efa67d56f
suresh <sureshkmr123789@gmail.com>
2024-05-21 21:47:27 +0530

    shell practice

--- 13-forloop_pack_install.sh
+++ 13-forloop_pack_install.sh
@@ -35,6 +35,6 @@ do
         dnf install $i -y &>>$LOGFILE
         VALIDATE $? "$i installation"
     else
-        echo "$i already installed .... $Y SKIPPING $N"
+        echo "$i already installed .... $G SKIPPING $N"
     fi
 done
