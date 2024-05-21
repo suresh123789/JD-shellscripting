ee00d61c91e409fa06ceffb2e144bdc3070408af
suresh <sureshkmr123789@gmail.com>
2024-05-21 19:53:19 +0530

    shell practice

--- 12-colors.sh
+++ 12-colors.sh
@@ -4,20 +4,26 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d '.' -f1 )
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
+R=$(\e[31m)
+G=$(\e[32m)
+Y=$(\e[33m)
+N=$(\e[0m)
+
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 FAILED"
+        echo "$2 $R FAILED $N"
         #exit 1
     else
-        echo "$2 SUCCESS"
+        echo "$2 $G SUCCESS $N"
     fi
 }
 if [ $USERID -eq 0 ]
 then 
-    echo "you are super user"
+    echo "$G you are super user $N"
 else
-    echo "you must be super user to execute"
+    echo "$R you must be super user to execute $N"
     exit 1
 fi
 
