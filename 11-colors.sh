fcdec2ee657a62807e3e2c279c5a0da2648697a3
suresh <sureshkmr123789@gmail.com>
2024-05-21 19:59:18 +0530

    shell practice

--- 11-colors.sh
+++ 11-colors.sh
@@ -13,17 +13,17 @@ N=$(\e[0m)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 $R FAILED $N"
+        echo -e "$2 $R FAILED $N"
         #exit 1
     else
-        echo "$2 $G SUCCESS $N"
+        echo -e "$2 $G SUCCESS $N"
     fi
 }
 if [ $USERID -eq 0 ]
 then 
-    echo "$G you are super user $N"
+    echo -e "$G you are super user $N"
 else
-    echo "$R you must be super user to execute $N"
+    echo -e "$R you must be super user to execute $N"
     exit 1
 fi
 
