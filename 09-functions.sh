8b160d311579bbf7b04fa57962af79b96518466a
suresh <sureshkmr123789@gmail.com>
2024-05-21 17:49:48 +0530

    shell practice

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 FAILED"
+    else
+        echo "$2 SUCCESS"
+        exit 1
+    fi
+}
+if [ $USERID -eq 0 ]
+then 
+    echo "you are super user"
+else
+    echo "you must be super user to execute"
+    exit 1
+fi
+
+dnf install mysql -y
+VALIDATE $? "mysql installation"
+
+
+dnf install git -y
+VALIDATE $? "mysql installation"
